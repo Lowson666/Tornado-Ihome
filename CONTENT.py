3ef9a6d5a74f67dbadee04ff378920db739e1675
Lowson <547805646@qq.com>
2018-06-11 22:10:49 +0800

    添加一个常量

--- CONTENT.py
+++ CONTENT.py
@@ -1,8 +1,12 @@
 # coding:utf-8
 
 # 验证码图片有效期/秒
-IMAGE_CODE_EXCIST_TIME = 6000
+IMAGE_CODE_EXCIST_TIME = 60
 # 短信验证码有效期/秒
-SMS_CODE_EXCIST_TIME = 3000
+SMS_CODE_EXCIST_TIME = 300
 # session有效期/秒
 SESSION_EXCIST_TIME = 86400
+# 七牛云链接
+QINIU_IMAGE_LINK = '//pa1mgprez.bkt.clouddn.com'
+# 城区信息有效期/秒
+REDIS_AREA_INFO_EXPIRES_SECONDES = 86400
